684f23cb34c5de0c253be8dd8229dd3ca813bcd1
franckymougel <francky.mougel@gmail.com>
2022-01-11 16:44:33 -0500

    animation

--- lelab/Views/TabVoiture.xaml.cs
+++ lelab/Views/TabVoiture.xaml.cs
@@ -10,6 +10,25 @@ namespace lelab.Views
         public TabVoiture()
         {
             InitializeComponent();
+            voitureImage.Opacity = 0;
+            voitureImage.TranslationX = 0;
+
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            voitureImage.TranslationX=0;
+            voitureImage.Opacity = 0;
+            voitureImage.FadeTo(1, 1000);
+            voitureImage.TranslateTo(50, 0, 1000,Easing.CubicOut);
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            voitureImage.Opacity = 0;
+            voitureImage.TranslationX = 0;
         }
     }
 }
